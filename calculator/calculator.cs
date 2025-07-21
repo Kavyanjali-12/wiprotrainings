d8b4a8f570e30ed9895e6be5a661407549d68bc1
Kavyanjali-12 <anjalikavya286@gmail.com>
2025-07-21 18:19:28 +0530

    first commit

---
+++ calculator/calculator.cs
@@ -0,0 +1,18 @@
+﻿using system;
+
+naming CalculatorOperations;
+
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the C sharp");
+        Console.ReadKey();
+
+
+    }
+        
+        
+    }
+}
